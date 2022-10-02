2c43a9efd44c37fc06f53a29f1a69a7a24979d4d
odedmasala <odedmasala6@gmail.com>
2022-10-02 11:22:49 +0300

    start app

---
+++ client/src/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import reactDOM from "react-dom/client";
+import "./index.css"
+import App from "./App";
+
+const root = reactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
