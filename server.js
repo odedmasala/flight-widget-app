e29fb05efdc31833a98abd6bea1e827a5c13fdf4
odedmasala <odedmasala6@gmail.com>
2022-10-02 16:35:19 +0300

    update

---
+++ server.js
@@ -0,0 +1,28 @@
+const PORT = 8000
+const axios = require('axios')
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+
+const app = express()
+
+
+app.get('/flights', (req, res) => {
+    const options = {
+        method: 'GET',
+        url: `${process.env.URL}?page-size=6`,
+        headers: {
+          accept: 'application/json',
+          'X-Cassandra-Token':process.env.TOKEN
+        }
+      };
+      
+      axios.request(options).then(function (response) {
+        console.log(response.data);
+        res.json(response.data)
+      }).catch(function (error) {
+        console.error(error);
+      });
+})
+
+app.listen(PORT, () => console.log(`running on port localhost:${PORT}/flights`))
